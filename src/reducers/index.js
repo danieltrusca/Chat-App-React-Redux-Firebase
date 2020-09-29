b5c16441c246c9976d887591aa18d25b24c65e3c
Dan Trusca <truscadani@yahoo.com>
2020-09-29 19:22:06 +0300

    adding&displaying public channels

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -23,8 +23,25 @@ const user_reducer = (state = initialUserState, action) => {
   }
 };
 
+const initialChannelState = {
+  currentChannel: null,
+};
+
+const channel_reducer = (state = initialChannelState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_CURRENT_CHANNEL:
+      return {
+        ...initialChannelState,
+        currentChannel: action.payload.currentChannel,
+      };
+    default:
+      return state;
+  }
+};
+
 const rootReducer = combineReducers({
   user: user_reducer,
+  channel: channel_reducer,
 });
 
 export default rootReducer;
