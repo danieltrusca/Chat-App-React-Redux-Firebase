d7d3ccf505e0a6d5bf66249f4c4971ad71de992c
Dan Trusca <truscadani@yahoo.com>
2020-09-30 10:40:25 +0300

    adding and displaying messages on public channels

---
+++ src/components/Messages/MessageForm.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Segment, Button, Input } from "semantic-ui-react";
+import firebase from "../../firebase";
+import FileModal from "./FileModal";
+
+class MessageForm extends React.Component {
+  state = {
+    message: "",
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    loading: false,
+    errors: [],
+    modal: false,
+  };
+
+  openModal = () => this.setState({ modal: true });
+
+  closeModal = () => this.setState({ modal: false });
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  createMessage = () => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: this.state.user.uid,
+        name: this.state.user.displayName,
+        avatar: this.state.user.photoURL,
+      },
+      content: this.state.message,
+    };
+    return message;
+  };
+
+  sendMessage = () => {
+    const { messagesRef } = this.props;
+    const { message, channel } = this.state;
+    if (message) {
+      this.setState({ loading: true });
+      messagesRef
+        .child(channel.id)
+        .push()
+        .set(this.createMessage())
+        .then(() => {
+          this.setState({ loading: false, message: "", errors: [] });
+        })
+        .catch((err) => {
+          console.error(err);
+          this.setState({
+            loading: false,
+            errors: this.state.errors.concat(err),
+          });
+        });
+    } else {
+      this.setState({
+        errors: this.state.errors.concat({ message: "Add a message" }),
+      });
+    }
+  };
+  render() {
+    const { errors, message, loading, modal } = this.state;
+    return (
+      <Segment className="message__form">
+        <Input
+          fluid
+          name="message"
+          value={message}
+          onChange={this.handleChange}
+          style={{ marginBottom: "0.7em" }}
+          label={<Button icon={"add"} />}
+          labelPosition="left"
+          className={
+            errors.some((error) => error.message.includes("message"))
+              ? "error"
+              : ""
+          }
+          placeholder="Write your message"
+        />
+        <Button.Group icon widths="2">
+          <Button
+            onClick={this.sendMessage}
+            disabled={loading}
+            color="orange"
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+          />
+          <Button
+            onClick={this.openModal}
+            color="teal"
+            content="Upload Media"
+            labelPosition="right"
+            icon="cloud upload"
+          />
+        </Button.Group>
+        <FileModal modal={modal} closeModal={this.closeModal} />
+      </Segment>
+    );
+  }
+}
+
+export default MessageForm;
