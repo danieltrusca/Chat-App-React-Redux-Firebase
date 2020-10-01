7173294d52c709bcb471f66d2e8af174bdadea56
Dan Trusca <truscadani@yahoo.com>
2020-10-01 16:10:52 +0300

    metaPanel component to display channel Info

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
     <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
@@ -23,7 +23,12 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
     </Grid.Column>
 
     <Grid.Column width={4}>
-      <MetaPanel />
+      <MetaPanel
+        key={currentChannel && currentChannel.id}
+        isPrivateChannel={isPrivateChannel}
+        currentChannel={currentChannel}
+        userPosts={userPosts}
+      />
     </Grid.Column>
   </Grid>
 );
@@ -32,6 +37,7 @@ const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
   isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts,
 });
 
 export default connect(mapStateToProps)(App);
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Segment, Comment } from "semantic-ui-react";
 import firebase from "../../firebase";
-
+import { setUserPosts } from "../../actions/index";
+import { connect } from "react-redux";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
 import Message from "./Message";
@@ -46,6 +47,7 @@ class Messages extends React.Component {
         messagesLoading: false,
       });
       this.countUniqueUsers(loadedMessages);
+      this.countUserPosts(loadedMessages);
     });
   };
 
@@ -139,6 +141,21 @@ class Messages extends React.Component {
     this.setState({ numUniqueUsers });
   };
 
+  countUserPosts = (messages) => {
+    let userPosts = messages.reduce((acc, message) => {
+      if (message.user.name in acc) {
+        acc[message.user.name].count += 1;
+      } else {
+        acc[message.user.name] = {
+          avatar: message.user.avatar,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+    this.props.setUserPosts(userPosts);
+  };
+
   displayMessages = (messages) =>
     messages.length > 0 &&
     messages.map((message) => (
@@ -199,4 +216,4 @@ class Messages extends React.Component {
   }
 }
 
-export default Messages;
+export default connect(null, { setUserPosts })(Messages);
